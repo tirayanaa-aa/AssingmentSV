ccbdb62e12756676fddf5b6228470b89861974f1
tirayanaa-aa <tirayanaa@gmail.com>
2025-11-02 01:22:03 +0800

    Update objective2.py

--- objective2.py
+++ objective2.py
@@ -1,1 +1,11 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
 
+st.header("Analysis of Students’ Satisfaction with Online Learning During COVID-19")
+
+# Load CSV from GitHub
+url = "https://raw.githubusercontent.com/tirayanaa-aa/AssingmentSV/refs/heads/main/processed_data.csv"
+
+# Read the dataset
+df = pd.read_csv(url)
