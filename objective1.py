99af8c7d9150cc72ac41e532d3620d408e446170
tirayanaa-aa <tirayanaa@gmail.com>
2025-11-02 01:19:54 +0800

    Update objective1.py

--- objective1.py
+++ objective1.py
@@ -1,1 +1,11 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
 
+st.header("Prior Academic Standing and Study Habits")
+
+# Load CSV from GitHub
+url = "https://https://raw.githubusercontent.com/tirayanaa-aa/AssingmentSV/refs/heads/main/processed_data.csv"
+
+# Read the dataset
+df = pd.read_csv(url)
