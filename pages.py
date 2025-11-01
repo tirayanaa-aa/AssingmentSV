795c297d057ec88b43664cb8b10d043111c7fc86
tirayanaa-aa <tirayanaa@gmail.com>
2025-11-02 01:25:30 +0800

    Update pages.py

--- pages.py
+++ pages.py
@@ -4,15 +4,15 @@ st.set_page_config(
     page_title="Student Performance Metrics"
 )
 
-page1 = st.Page('objective1.py', title='PRIOR ACADEMIC & HABITS', icon=":material/thumb_up_off_alt:")
+objective1 = st.Page('objective1.py', title='PRIOR ACADEMIC & HABITS', icon=":material/thumb_up_off_alt:")
 
-page2 = st.Page('objective2.py', title='DEMOGRAPHIC & SOCIOECONOMIC', icon=":material/assignment_turned_in:")
+objective2 = st.Page('objective2.py', title='DEMOGRAPHIC & SOCIOECONOMIC', icon=":material/assignment_turned_in:")
 
 home = st.Page('Homepage.py', title='Homepage', default=True, icon=":material/home:")
 
 pg = st.navigation(
         {
-            "Menu": [home, page1, page2]
+            "Menu": [home, objective1, objective2]
         }
     )
 
