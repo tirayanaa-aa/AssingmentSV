242ac366bbe8b5a1870adccea1606456367a749f
tirayanaa-aa <tirayanaa@gmail.com>
2025-11-01 21:59:09 +0800

    Update pages.py

--- pages.py
+++ pages.py
@@ -1,1 +1,19 @@
+import streamlit as st
 
+st.set_page_config(
+    page_title="Student Performance Metrics"
+)
+
+page1 = st.Page('StudentSatisfaction.py', title='Kepuasan Pelajar Dalam Pembelajaran', icon=":material/thumb_up_off_alt:")
+
+page2 = st.Page('PerformanceImpact.py', title='Kesan Dalam Pembelajaran', icon=":material/assignment_turned_in:")
+
+home = st.Page('Homepage.py', title='Homepage', default=True, icon=":material/home:")
+
+pg = st.navigation(
+        {
+            "Menu": [home, page1, page2]
+        }
+    )
+
+pg.run()
