f8bcef9807f9538bc0877419d0159b1bd84208d1
tirayanaa-aa <tirayanaa@gmail.com>
2025-11-02 01:58:24 +0800

    Update pages.py

--- pages.py
+++ pages.py
@@ -8,11 +8,13 @@ objective1 = st.Page('objective1.py', title='PRIOR ACADEMIC & HABITS', icon=":ma
 
 objective2 = st.Page('objective2.py', title='DEMOGRAPHIC & SOCIOECONOMIC', icon=":material/assignment_turned_in:")
 
+objective3 = st.Page('objective3.py', title='TEMPORAL & HABIT INTERACTION', icon=":material/schedule:")
+
 home = st.Page('Homepage.py', title='Homepage', default=True, icon=":material/home:")
 
 pg = st.navigation(
         {
-            "Menu": [home, objective1, objective2]
+            "Menu": [home, objective1, objective2, objective3]
         }
     )
 
